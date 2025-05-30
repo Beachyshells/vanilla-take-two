4d788e7c4a63a88b0b82c3fd63366f8887bab5dd
Beachyshells <5beachflower5@gmail.com>
2025-05-30 15:48:00 -0400

    updated the search engine with API
    Now, when the input value is added, the city information is console logged. I can now update the html of the weather items.

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,20 @@
-function handleSearchSubmit(event) {
+function searchCity(city) {
+  let apiKey = "b9aaeaaf97004f2a03afob830bt63baf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+  console.log(response.data);
+}
+
+function pushSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#user-input");
   let searchedCityElement = document.querySelector("#searched-city");
   searchedCityElement.innerHTML = searchInput.value;
-
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${}&appid=${apiKey}&units=metric`;
-let apiKey="b9aaeaaf97004f2a03afob830bt63baf"
+searchFormElement.addEventListener("submit", pushSubmit);
