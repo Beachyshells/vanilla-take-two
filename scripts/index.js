8bed4b4f902ae32870b41ff776dc1dcd840acf24
Beachyshells <5beachflower5@gmail.com>
2025-05-30 15:04:52 -0400

    added my weather API
    added the SheCodes weather API to get the current weather from any city.

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,12 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#user-input");
   let searchedCityElement = document.querySelector("#searched-city");
   searchedCityElement.innerHTML = searchInput.value;
+
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${}&appid=${apiKey}&units=metric`;
+let apiKey="b9aaeaaf97004f2a03afob830bt63baf"
