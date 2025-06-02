efedfd1f29f040f0075ae5d39b51a23f608e6af7
Beachyshells <5beachflower5@gmail.com>
2025-06-01 21:43:05 -0400

    i have updated the html again working on the css.
    im going to push what i have. this top weather information is not lining up like I would like.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-function updateTime() {
-  let now = new Date();
+function updateTime(unixTime) {
+  let cityTime = new Date(unixTime * 1000);
 
   let days = [
     "Sunday",
@@ -10,29 +10,40 @@ function updateTime() {
     "Friday",
     "Saturday",
   ];
-  let currentDay = days[now.getDay()];
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
+  let currentDay = days[cityTime.getDay()];
+  let hours = cityTime.getHours();
+  let minutes = cityTime.getMinutes();
 
   let ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12 || 12;
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
-  let formattedTime = `${currentDay} ${hours}:${minutes} ${ampm}`;
-  document.getElementById("current-time").innerHTML = formattedTime;
+  document.getElementById("todays-day").innerHTML = currentDay;
+  document.getElementById("current-hours").innerHTML = hours;
+  document.getElementById("current-minutes").innerHTML = `${minutes} ${ampm}`;
 }
-updateTime();
-setInterval(updateTime, 60000);
 
+function capitalizeWords(input) {
+  return input
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 function searchCity(city) {
   let apiKey = "b9aaeaaf97004f2a03afob830bt63baf";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
   axios.get(apiUrl).then(displayWeather);
 }
 
 function displayWeather(response) {
   console.log(response.data);
+
+  let searchedCityElement = document.querySelector("#searched-city");
+  searchedCityElement.innerHTML = response.data.city;
 
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -41,19 +52,32 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
 
-  let windElement = document.querySelector("#wind");
+  let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
 
   let iconUrlElement = document.querySelector("#weather-icon");
   iconUrlElement.src = response.data.condition.icon_url;
+  iconUrlElement.style.display = "inline";
+
+  let farenheitElement = document.querySelector(".degree-fahrenheit");
+  farenheitElement.style.display = "inline";
+  updateTime(response.data.time);
+  if (window.timeUpdateInterval) {
+    clearInterval(window.timeUpdateInterval);
+  }
+  window.timeUpdateInterval = setInterval(() => {
+    response.data.time += 60;
+    updateTime(response.data.time);
+  }, 60000);
 }
 
 function pushSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#user-input");
-  let searchedCityElement = document.querySelector("#searched-city");
-  searchedCityElement.innerHTML = searchInput.value;
-  searchCity(searchInput.value);
+  let city = capitalizeWords(searchInput.value.trim());
+  if (city) {
+    searchCity(city);
+  }
 }
 
 let searchFormElement = document.querySelector("#search-form");
