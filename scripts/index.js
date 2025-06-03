9daa871a3248350df1f05252bf513d6bdee3a3da
Beachyshells <5beachflower5@gmail.com>
2025-06-03 15:21:47 -0400

    Finalized all css and javascript for my vanilla weather app
    I am pretty happy with my first real project!!!!

--- scripts/index.js
+++ scripts/index.js
@@ -110,26 +110,39 @@ function getForecast(city) {
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
 function displayForecast(response) {
   console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div class="weather-forecast-icon" >
+    <img src="${day.condition.icon_url}" />
+      </div>
     <div class="weather-forecast-temperatures">
-    <div class="weather-forecast-temperature">
-    <strong>68°</strong>
+    <div class="weather-forecast-temperature" >
+    <strong class="stronger">${Math.round(day.temperature.maximum)}°  </strong>
     </div>
-    <div class="weather-forecast-temperature">53°</div>
+    <div class="weather-forecast-temperature">${Math.round(
+      day.temperature.minimum
+    )}°</div>
     </div>
     </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
