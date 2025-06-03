d4b2e1a9b8d89e9695d7f630c3694ad5eb0a37c5
Beachyshells <5beachflower5@gmail.com>
2025-06-03 12:45:33 -0400

    wrote html and js for displaying the forecast
    styled the project, will continue styling and adding api to the weather forecast

--- scripts/index.js
+++ scripts/index.js
@@ -101,5 +101,32 @@ function pushSubmit(event) {
   }
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = " ";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+    <strong>68°</strong>
+    </div>
+    <div class="weather-forecast-temperature">53°</div>
+    </div>
+    </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", pushSubmit);
+
+displayForecast();
